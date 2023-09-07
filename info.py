e744ce2a205bbc89f2f371bc57a8c48c8a4fd415
parshucr123 <118430657+parshucr123@users.noreply.github.com>
2023-09-07 22:08:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,8 +43,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Srmoviezbot:Srmoviezbot@cluster0.46rgafa.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Bisal") #don't change the name if you are using the database already
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Rsmoviezbot:<Rsmoviezbot>@cluster0.uqrc0ga.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Rsmoviez Bot") #don't change the name if you are using the database already
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
